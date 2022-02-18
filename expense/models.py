74bda947529d093848ca461dd7a03de1f5df5d11
prabesh uprety <prabeshuprety1@gmail.com>
2022-02-18 23:20:27 +0545

    added user expenses

--- expense/models.py
+++ expense/models.py
@@ -1,10 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class expenses(models.Model):
     reason = models.CharField(max_length=30)
     date = models.DateTimeField(auto_now=True)
     amount = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+
 
     def __str__(self):
         return str(self.reason)
--- expense/views.py
+++ expense/views.py
@@ -1,21 +1,21 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from .models import expenses
+from django.contrib.auth.decorators import login_required
 
-
-
+@login_required
 def exp(request):
     expense = expenses()
-    data = expenses.objects.all()
+    data = expenses.objects.filter(user=request.user)
     total = 0
     for amount in data:
         total += amount.amount
     if request.POST.get('reason'):
         expense.reason = request.POST.get('reason')
         expense.amount = request.POST.get('amount')
-        
+        expense.user = request.user
         expense.save()
-        return render(request, 'expense/expense.html', {'data': data, 'total': total})
+        return redirect('expense')
     return render(request, 'expense/expense.html', {'data': data,'total': total})
 
 
